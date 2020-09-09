fe55df481be4388cdcb82efe3e784490d999c5fb
MuhHanif <48MuhHanif@gmail.com>
2020-09-09 09:42:40 +0700

    added feature for read from csv

--- word_automata_v0.1.py
+++ word_automata_v0.1.py
@@ -1,5 +1,5 @@
 from docx import Document as dc
-
+import pandas as pd
 
 
 class editor(object):
@@ -10,10 +10,7 @@ class editor(object):
         #set global variable
         pass
 
-
-
-    def entryTemplateFieldContent(self, dir):
-
+    def entryTemplateFieldContent(self, dir, documentName, csvData):
 
         #read document
         doc = dc(dir)
@@ -21,25 +18,55 @@ class editor(object):
         #count paragraph
         pGraphCount = len(doc.paragraphs)
 
-        #read all paragraph
-        for paragraph in range(0, pGraphCount):
+        #get this data from csv and loop
 
-            #count run
-            runCount = len(doc.paragraphs[paragraph].runs)
+        replacementFile = pd.read_csv(csvData, header=None)
 
-            #read all
-            for run in range(0, runCount):
+        print(replacementFile)
 
-                #print paragraph to cmd
-                print(paragraph," ",run," ","[" + doc.paragraphs[paragraph].runs[run].text + "]")
+        for x in range(len(replacementFile.iloc[:,0])):
 
-                pass
+            replaceFieldFound = replacementFile.iloc[x, 0]
+            textReplacement = replacementFile.iloc[x, 1]
+            print(replaceFieldFound)
+            print(textReplacement)
+
+            #read all paragraph
+            for paragraph in range(0, pGraphCount):
+
+                #get each paragraph
+                pGraph = doc.paragraphs[paragraph]
+
+                #count run
+                runCount = len(pGraph.runs)
+
+                #read all
+                for run in range(0, runCount):
+
+                    #get each run
+                    runData = pGraph.runs[run]
+
+                    #replace field with desired text
+                    if replaceFieldFound in runData.text:
+
+                        #notification
+                        print("[",replaceFieldFound,"terganti dengan", textReplacement,"]")
+                        runData.text = runData.text.replace(replaceFieldFound, textReplacement)
+                        print(runData.text)
+
+                        #print paragraph to cmd
+                        print(paragraph," ",run," ","[" + doc.paragraphs[paragraph].runs[run].text + "]")
+
+                        pass
+
+                        pass
 
             pass
+        doc.save(documentName)
 
         pass
 
 #document directory
 data = input("masukkan directory word: ")
 
-file = editor().entryTemplateFieldContent(data)
+file = editor().entryTemplateFieldContent(data, "result.docx", "replace.csv")

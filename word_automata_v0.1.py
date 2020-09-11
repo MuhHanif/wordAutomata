b6c23dd16109ec36fb5763fd2bfdb7398c1809b7
MuhHanif <48MuhHanif@gmail.com>
2020-09-11 09:28:07 +0700

    added replace table data features

--- word_automata_v0.1.py
+++ word_automata_v0.1.py
@@ -12,26 +12,20 @@ class editor(object):
 
     def entryTemplateFieldContent(self, dir, csvData):
 
-
         #get this data from csv and loop
         readExcel = xlrd.open_workbook(csvData)
         sheet = readExcel.sheet_by_index(0)
 
-
-        #loop through csv data
-
-        #iterate through rows
+        #iterate through data entry excel rows
         for x in range(sheet.nrows - 1):
+
             #read document
             doc = dc(dir)
-
-            #count paragraph
-            pGraphCount = len(doc.paragraphs)
 
             fileName = sheet.cell_value(x + 1,0)
             print(fileName)
 
-            #iterate through columns
+            #iterate through data entry excel columns
             for cols in range(sheet.ncols):
 
                 #read specific data from csv
@@ -42,27 +36,19 @@ class editor(object):
                 #print(replaceFieldFound)
                 #print(textReplacement)
 
-                #read all paragraph
-                for paragraph in range(0, pGraphCount):
+                #read all paragraph (mailmerge mode)
+                for paragraph in doc.paragraphs:
 
-                    #get each paragraph
-                    pGraph = doc.paragraphs[paragraph]
-
-                    #count run
-                    runCount = len(pGraph.runs)
-
-                    #read all
-                    for run in range(0, runCount):
-
-                        #get each run
-                        runData = pGraph.runs[run]
+                    #read all runs
+                    for run in paragraph.runs:
 
                         #replace field with desired text
-                        if replaceFieldFound in runData.text:
+                        if replaceFieldFound in run.text:
 
                             #notification
-                            print("paragraph",paragraph,"run",run,"[",replaceFieldFound,"terganti dengan", textReplacement,"]")
-                            runData.text = runData.text.replace(replaceFieldFound, str(textReplacement))
+                            #"paragraph",paragraph,"run",run,
+                            print("[",replaceFieldFound,"terganti dengan", textReplacement,"]")
+                            run.text = run.text.replace(replaceFieldFound, str(textReplacement))
 
                             pass
 
@@ -72,9 +58,101 @@ class editor(object):
 
                 pass
 
+            #create new file
             doc.save(fileName + ".docx")
             print("done! created file as",fileName + ".docx")
-            #some bug casues print fist row only !
+            #some bug casues print fist row only ! (Solved!)
+            pass
+
+        pass
+
+    def entryTemplateFieldContentTable(self, dir, csvData):
+
+        #get this data from csv and loop
+        readExcel = xlrd.open_workbook(csvData)
+        sheet = readExcel.sheet_by_index(0)
+
+        #iterate through data entry excel rows
+        for x in range(sheet.nrows - 1):
+
+            #read document
+            doc = dc(dir)
+
+            fileName = sheet.cell_value(x + 1,0)
+            print(fileName)
+
+            #iterate through data entry excel columns
+            for cols in range(sheet.ncols):
+
+                #read specific data from csv
+                replaceFieldFound = sheet.cell_value(0, cols) #this value holds
+                textReplacement = sheet.cell_value(x + 1, cols) #this value varies
+
+                #for debug only
+                #print(replaceFieldFound)
+                #print(textReplacement)
+
+                #replace table (mailmerge table data)
+                for table in doc.tables:
+
+                    for row in table.rows:
+
+                        for cell in row.cells:
+
+                            #read all paragraph (mailmerge mode)
+                            for paragraph in cell.paragraphs:
+
+                                #read all runs
+                                for run in paragraph.runs:
+
+                                    #print(run.text)
+
+                                    #replace field with desired text
+                                    if replaceFieldFound in run.text:
+
+                                        #notification
+                                        #"paragraph",paragraph,"run",run,
+                                        print("[",replaceFieldFound,"terganti dengan", textReplacement,"]")
+                                        run.text = run.text.replace(replaceFieldFound, str(textReplacement))
+
+                                        pass
+
+                                    pass
+
+                                pass
+
+                            pass
+
+                        pass
+
+                    pass
+
+                #read all paragraph (mailmerge mode)
+                for paragraph in doc.paragraphs:
+
+                    #read all runs
+                    for run in paragraph.runs:
+
+                        #replace field with desired text
+                        if replaceFieldFound in run.text:
+
+                            #notification
+                            #"paragraph",paragraph,"run",run,
+                            print("[",replaceFieldFound,"terganti dengan", textReplacement,"]")
+                            run.text = run.text.replace(replaceFieldFound, str(textReplacement))
+
+                            pass
+
+                        pass
+
+                    pass
+
+                pass
+
+            #create new file
+            doc.save(fileName + ".docx")
+            print("done! created file as",fileName + ".docx")
+            #some bug casues print fist row only ! (Solved!)
             pass
 
         pass
@@ -94,7 +172,7 @@ while True:
 
         #output = input("nama file output: ")
 
-        file = editor().entryTemplateFieldContent(data, replace)
+        file = editor().entryTemplateFieldContentTable(data, replace)
 
         pass
 
